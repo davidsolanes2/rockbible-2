2d20eed84d1eaa72f688735b667884aa26514f6b
Bryan A. Huto Toctaguano <bryananthony.ht@gmail.com>
2017-11-10 09:22:44 +0100

    Imports corregidos.

--- src/main/java/com/mycompany/myapp/web/rest/SongResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/SongResource.java
@@ -10,6 +10,7 @@ import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
